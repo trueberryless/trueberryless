e9e49a085b6e644765df8fd5e765e558f70b0d4c
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-06-15 17:26:59 +0200

    refactor: better automated setup

---
+++ scripts/build.ts
@@ -0,0 +1,48 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const BENTO_HTML_PATH = path.join(__dirname, "../src/bento-grid.html");
+const OUTPUT_SVG_PATH = path.join(__dirname, "../html-wrapper.svg");
+const LOCAL_SNAKE_PATH = path.join(
+  __dirname,
+  "../dist/github-contribution-grid-snake-dark.svg",
+);
+
+const SNAKE_URL =
+  "https://raw.githubusercontent.com/trueberryless/trueberryless/snake/github-contribution-grid-snake-dark.svg";
+const SPOTIFY_URL =
+  "https://spotify-github-profile.kittinanx.com/api/view?uid=pjyifjjapvah0yoy2zvd8vx0v&cover_image=true&theme=default&show_offline=false&background_color=0d1117&interchange=true&bar_color=53b14f&bar_color_cover=true";
+
+async function build() {
+  let html = fs.readFileSync(BENTO_HTML_PATH, "utf-8");
+
+  let snakeSvg = "";
+  if (fs.existsSync(LOCAL_SNAKE_PATH)) {
+    snakeSvg = fs.readFileSync(LOCAL_SNAKE_PATH, "utf-8");
+  } else {
+    const snakeRes = await fetch(SNAKE_URL);
+    snakeSvg = await snakeRes.text();
+  }
+
+  const spotifyRes = await fetch(SPOTIFY_URL);
+  let spotifySvg = await spotifyRes.text();
+
+  spotifySvg = spotifySvg.replace(/<a[^>]*>/g, "").replace(/<\/a>/g, "");
+
+  html = html.replace("{{SNAKE_SVG}}", snakeSvg);
+  html = html.replace("{{SPOTIFY_SVG}}", spotifySvg);
+
+  const finalSvg = `<svg width="980" height="913" xmlns="http://www.w3.org/2000/svg">
+    <foreignObject width="980" height="913">
+        ${html}
+    </foreignObject>
+</svg>`;
+
+  fs.writeFileSync(OUTPUT_SVG_PATH, finalSvg);
+}
+
+build();
